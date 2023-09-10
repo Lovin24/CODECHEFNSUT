07eab2ef8c0f565ac653e7037b78eddae0d17ebe
Lovin24 <127424651+Lovin24@users.noreply.github.com>
2023-09-10 17:19:51 +0530

    comments_1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,20 @@
+//somebody else might have written a simpler code but this works as well ○)
 #include<iostream>
 using namespace std;
 int main(){
-    int n;cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int space=1;space<=n-i;space++)cout<<" ";
-        int tempnum=i;
-        for(int j=1;j<=i;j++){
+    int n;cin>>n; //Taking input for rows
+    for(int col=1;col<=n;col++){   //looping for colums
+        for(int space=1;space<=n-col;space++)cout<<" "; //looping for spaces 
+                                                        //observing space is 'col' less than the column
+        int tempnum=col-1;     //initialized a variable for printing num
+        for(int j=1;j<=col;j++){ // looping for printing till the middle part
 
-            cout<<tempnum++;
+            cout<<++tempnum;
         }
+                                //! WE HAVE USED ONLY SINGLE VARIABLE TO KEEP TRACK OF PRINTIN NUMBERS 
         
-        tempnum--;
-        for(int j=1;j<=i-1;j++){
+        for(int j=1;j<=col-1;j++){ //looping for printing the other numbers after middle part
+
             cout<<--tempnum;
 
         }
